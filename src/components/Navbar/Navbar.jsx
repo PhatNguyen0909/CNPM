2a09035b5911da113c6018cc644640f0f85fcbe9
Kiet Lieu <tuankiet9804@gmail.com>
2025-09-17 21:33:29 +0700

    Xong navbar rồi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
             <img src = {assets.basket_icon} alt = " "/>
             <div className="dot">
             </div>
+            
         </div>
         <button>sign in</button>
       </div>
