7d66ab7ffbb03ac503aa1512abe65fd0b0d66a4e
PhatNguyen0909 <nhiknh112233@gmail.com>
2025-09-22 00:08:38 +0700

    update fooditem

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
-
-const FoodItem = ({id,name,address,description,image,rating,cuisine}) => {
+const FoodItem = ({id,restaurantId,name,price,description,image}) => {
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
@@ -11,15 +10,11 @@ const FoodItem = ({id,name,address,description,image,rating,cuisine}) => {
         <div className='food-item-info'>
            <div className='food-item-name-rating'>
               <p>{name}</p>
-              <div className="rating">
-                <img src={assets.rating_starts} alt = ""/>
-                <span>{rating}</span>
-              </div>
+              <img src={assets.rating_starts} alt = ""/>
            </div>
-           <p className='food-item-cuisine'>{cuisine}</p>
            <p className='food-item-desc'>{description}</p>
-           <p className='food-item-address'>{address}</p>
         </div>
+      
     </div>
   )
 }
